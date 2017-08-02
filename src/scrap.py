a4ee7027bb56be784258f9bef6d46e387799e573
Chinmay Narayan <chinu.pandey@gmail.com>
2017-08-02 10:43:08 +0530

    Initial commit

---
+++ src/scrap.py
@@ -0,0 +1,11 @@
+
+
+#for each directory present in resrouces/scraped
+#for each file present in that directory, all those html's belong to one questions and
+#they should be read in the increasing order of numbers
+
+
+##if the file name is 1.html then get the question from there and store in question table.
+
+##else get the selectors of the answers and put them together in some dataframe
+##we can find questions here as well.. prepare the tree of such question answers..
